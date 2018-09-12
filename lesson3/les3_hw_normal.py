56e1e26369b5541b0f714c8af159620c65166103
usermuser <lwrdrstv@gmail.com>
2018-09-12 21:43:35 +0500

    finished Exercises in normal level

--- lesson3/les3_hw_normal.py
+++ lesson3/les3_hw_normal.py
@@ -4,50 +4,62 @@
 # Запишите результаты в файл salary.txt так, чтобы на каждой строке было 2 столбца,
 # столбцы разделяются пробелом, тире, пробелом. в первом имя, во втором зарплата, например: Vasya - 5000
 
-# После чего прочитайте файл, выведите построчно имя и зарплату минус 13% (налоги ведь),
-# Есть условие, не отображать людей получающих более зарплату 500000, как именно
-#  выполнить условие решать вам, можете не писать в файл
-# можете не выводить, подумайте какой способ будет наиболее правильным и оптимальным,
-#  если скажем эти файлы потом придется передавать.
-# Так же при выводе имя должно быть полностью в верхнем регистре!
-# Подумайте вспоминая урок, как это можно сделать максимально кратко, используя возможности языка Python.
 
 names = ['Денис', 'Сергей', 'Владимир', 'Гвидо']
-salaries = ['100500.00', '15000', '999999.99', '654321.321']
+salaries = ['100500.00', '15000', '100', '654321.321']
 
 person_card = dict(zip(names, salaries))
 print('person card =', person_card)
 
 with open('salary.txt', 'w', encoding='utf-8') as file:
     for name, salary in person_card.items():
+        print(name, salary)
         file.write('{} - {}\n'.format(name, salary))
+
 print('Конец первой части. Файл записан.')
 
-print('\nВторая часть задачи\n')
-with open('salary.txt', 'r', encoding='utf-8') as file:
-    line = file.read()
-    line = line.strip()
-    print(line)
-    # print('line strip', line.strip())
-print('\nКонец второй части.')
 
-# print('\n Вторая часть задачи решенная циклом\n')
+# После чего прочитайте файл, выведите построчно имя и зарплату минус 13% (налоги ведь),
+# Есть условие, не отображать людей получающих более зарплату 500000, как именно
+#  выполнить условие решать вам, можете не писать в файл
+# можете не выводить, подумайте какой способ будет наиболее правильным и оптимальным,
+#  если скажем эти файлы потом придется передавать.
+# Так же при выводе имя должно быть полностью в верхнем регистре!
+# Подумайте вспоминая урок, как это можно сделать максимально кратко,
+# используя возможности языка Python.
+
+# def lst_to_dict(*args):
+#     dict={}
+#     print('*args', args)
+#     for item in args:
+#         item = item.split('-')
+#         dict.update({item[0]:item[1].strip()})
+#     return dict
+#
+# print('\nВторая часть задачи\n')
 # with open('salary.txt', 'r', encoding='utf-8') as file:
-#     dict = {}
-    # lines = file.readline()
-    # for line in lines:
-    #     print(line)
-    # for line in file:
-
-        #line = line.split('-')  #line is list
-        #print(line[0], line[1])
-        # dict[line[0]] = line[1]
-        # print(dict)
-
+#     data_list = file.readlines()
+#     print('data_list = ', data_list)
+#     result = map(lst_to_dict, data_list)
+#     result_list = list(result)
+#     print(result_list)
+#     print('\nКонец второй части.')
 '''
-with open('salary.txt', 'w', encoding='utf-8') as file:
-    for name, salary in person_card.items():
-        salary = float(salary)              # конвертируем salary из string в float
-        salary -= salary*0.13               # вычитаем 13 процентов
-        file.write('{} - {}\n'.format(name, salary))
+with open('salary.txt', 'r', encoding='utf-8') as file:
+    dict = {}
+    print('marker')
+    for line in file.readlines():
+        key, val = line.strip().split('-')
+        dict[key] = val
 '''
+
+with open('salary.txt', 'r', encoding='utf-8') as file:
+    dict = {}
+    print('marker')
+    for line in file.readlines():
+        key, val = line.strip().split('-')
+        dict[key] = float(val)
+        dict[key] -= dict[key]*0.13
+    print(dict)
+
+
