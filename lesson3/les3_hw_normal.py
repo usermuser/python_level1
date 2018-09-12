5dbf59748f838478024befa0cd718ccf2d1ecd7e
usermuser <lwrdrstv@gmail.com>
2018-09-12 21:51:37 +0500

    Решаю normal

--- lesson3/les3_hw_normal.py
+++ lesson3/les3_hw_normal.py
@@ -16,28 +16,38 @@ names = ['Денис', 'Сергей', 'Владимир', 'Гвидо']
 salaries = ['100500.00', '15000', '999999.99', '654321.321']
 
 person_card = dict(zip(names, salaries))
-print(person_card)
+print('person card =', person_card)
 
 with open('salary.txt', 'w', encoding='utf-8') as file:
     for name, salary in person_card.items():
-        file.write(name + " - " + salary + "\n")
-        print(name, salary, sep=' - ')
+        file.write('{} - {}\n'.format(name, salary))
+print('Конец первой части. Файл записан.')
 
 print('\nВторая часть задачи\n')
 with open('salary.txt', 'r', encoding='utf-8') as file:
-    # lines = file.readlines()
-    # print(lines)
     line = file.read()
-    print(line.strip())
+    line = line.strip()
+    print(line)
+    # print('line strip', line.strip())
+print('\nКонец второй части.')
 
-print('\n Вторая часть задачи решенная циклом\n')
-with open('salary.txt', 'r', encoding='utf-8') as file:
-    dict={}
+# print('\n Вторая часть задачи решенная циклом\n')
+# with open('salary.txt', 'r', encoding='utf-8') as file:
+#     dict = {}
     # lines = file.readline()
     # for line in lines:
     #     print(line)
-    for line in file:
-        print(line.strip())
-        print(line.strip().split('-'))
-        dict[line[0]] = line[1]
-        print(dict)
+    # for line in file:
+
+        #line = line.split('-')  #line is list
+        #print(line[0], line[1])
+        # dict[line[0]] = line[1]
+        # print(dict)
+
+'''
+with open('salary.txt', 'w', encoding='utf-8') as file:
+    for name, salary in person_card.items():
+        salary = float(salary)              # конвертируем salary из string в float
+        salary -= salary*0.13               # вычитаем 13 процентов
+        file.write('{} - {}\n'.format(name, salary))
+'''
