62b50e29a86f5da5ad66e602f474a9a649f7bd82
usermuser <lwrdrstv@gmail.com>
2018-09-20 21:17:13 +0500

    hw5 done

--- lesson5/5_with_args.py
+++ lesson5/5_with_args.py
@@ -1,7 +1,32 @@
-# Данный скрипт можно запускать с параметрами:
-# python with_args.py param1 param2 param3
 import os
 import sys
+# rmdir or remove
+try:
+    import lesson5.hw05_easy_3 as my_lib
+    import lesson5.hw05_easy as my_lib2
+except ImportError:
+    import hw05_easy_3 as my_lib
+    import hw05_easy as my_lib2
+
+# Задание-1:
+# Доработайте реализацию программы из примера examples/5_with_args.py,
+# добавив реализацию следующих команд (переданных в качестве аргументов):
+#   cp <file_name> - создает копию указанного файла
+#   rm <file_name> - удаляет указанный файл (запросить подтверждение операции)
+#   cd <full_path or relative_path> - меняет текущую директорию на указанную
+#   ls - отображение полного пути текущей директории
+# путь считать абсолютным (full_path) -
+# в Linux начинается с /, в Windows с имени диска,
+# все остальные пути считать относительными.
+
+# Важно! Все операции должны выполняться в той директории, в который вы находитесь.
+# Исходной директорией считать ту, в которой был запущен скрипт.
+
+# P.S. По возможности, сделайте кросс-платформенную реализацию.
+
+# Данный скрипт можно запускать с параметрами:
+# python with_args.py param1 param2 param3
+
 print('sys.argv = ', sys.argv)
 
 
@@ -9,6 +34,10 @@ def print_help():
     print("help - получение справки")
     print("mkdir <dir_name> - создание директории")
     print("ping - тестовый ключ")
+    print("cp <file_name> - создать копию файла")
+    print("rm <dir_name> - удалить пустую папку или файл")
+    print("cd <full_path or relative_path> - меняет текущую папку на указанную")
+    print("ls - отображение полного пути текущей директории")
 
 
 def make_dir():
@@ -26,21 +55,64 @@ def make_dir():
 def ping():
     print("pong")
 
-'''
-cp <file_name> - создает копию указанного файла
-rm <file_name> - удаляет указанный файл (запросить подтверждение операции)
-cd <full_path or relative_path> - меняет текущую директорию на указанную
-ls - отображение полного пути текущей директории
-'''
+def copy_file():
+    if not file_name:
+        print("Необходимо указать имя файла вторым параметром")
+        return
+    file_path = os.path.join(os.getcwd(), file_name)
+    try:
+        my_lib.copy_file(file_name)
+        print('Файл с именем copy_{} успешно создан'.format(file_name))
+    except FileNotFoundError:
+        print('Файла с именем {} не существует'.format(file_name))
+
+def remove_file_or_dir():
+    if not dir_name:
+        print("Необходимо указать имя директории вторым параметром")
+        return
+
+    confirm = input('Вы уверены что хотите удалить {}? (Y/N)'.format(dir_name))
+    if confirm == 'Y':
+        dir_path = os.path.join(os.getcwd(), dir_name)
+        print(dir_name, '\n', dir_path)
+        if os.path.isdir(dir_path):
+            try:
+                os.rmdir(dir_path)
+                print('Директория {} удалена'.format(dir_name))
+            except OSError:
+                print('Директория с именем {} не пустая'.format(dir_name))
+        else:
+            try:
+                os.remove(dir_path)
+                print('Файл {} удален.'.format(dir_name))
+            except FileExistsError:
+                print('Такого файла нет')
+
+    elif confirm == 'N':
+        print('Удаление {} было отменено.'.format(dir_name))
+
+def ls():
+    print('\nПолный путь текущей директории:')
+    print(os.getcwd())
+    print('\nСписок папок и файлов текущей директории:\n', my_lib2.show_dirs_and_files())
+
+
+def cd():
+    if not dir_name:
+        print('\nНеобходимо указать имя директории вторым параметром')
+        return
+    os.chdir(dir_name)
+    print('\nВы перешли в директорию {}'.format(dir_name))
+    print('Полный путь:', os.getcwd())
 
 do = {
     "help": print_help,
     "mkdir": make_dir,
     "ping": ping,
-    "cp": pass,
-    "rm": pass,
-    "cd": pass,
-    "ls": pass,
+    "cp": copy_file,
+    "rm": remove_file_or_dir,
+    "cd": cd,
+    "ls": ls,
 }
 
 if __name__ == '__main__':
@@ -48,6 +120,11 @@ if __name__ == '__main__':
         dir_name = sys.argv[2]
     except IndexError:
         dir_name = None
+
+    try:
+        file_name = sys.argv[2]
+    except IndexError:
+        file_name = None
 
     try:
         key = sys.argv[1]
--- lesson5/hw05_easy_3.py
+++ lesson5/hw05_easy_3.py
@@ -4,13 +4,14 @@ import sys
 
 print('\nЗадача easy-3. Следует запускать из командной строки')
 
-filename = sys.argv[0]
+def copy_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        file_content = file.read()
+        # print(file_content)
 
-with open(filename, 'r', encoding='utf-8') as file:
-    file_content = file.read()
-    print(file_content)
+    with open('copy_' + filename, 'w', encoding='utf-8') as file:
+        file.write(file_content)
 
-with open('copy_' + filename, 'w', encoding='utf-8') as file:
-    file.write(file_content)
-
-
+if __name__ == '__main__':
+    filename = sys.argv[0]
+    copy_file(filename)
