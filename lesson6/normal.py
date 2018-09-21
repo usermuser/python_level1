8565f70acbc04ef0cc44d380601926e2155bcea0
usermuser <lwrdrstv@gmail.com>
2018-09-21 19:39:21 +0500

    created fight logic added function to calculate damage

--- lesson6/normal.py
+++ lesson6/normal.py
@@ -18,5 +18,33 @@ class Person():
         self.damage = damage
         self.armor = armor
 
-    def attack(self, attacker, victim):
 
+class Player(Person):
+    pass
+
+
+class Enemy(Person):
+    pass
+
+def attack(attacker, victim):
+    damage = calculate_damage(attacker.damage, victim.armor)
+    victim.health -= damage
+    print('{}')
+
+player = ''
+enemy = ''
+
+
+def start_game():
+    while player.health > 0 and enemy.health > 0:
+        if attacker = player:
+            attack(player, enemy)
+            attacker = enemy
+        else:
+            attack(enemy, player)
+            attacker = player
+
+    if player.health > 0:
+        print(player.name, 'победил!')
+    else:
+        print(enemy.name, 'победил!')
