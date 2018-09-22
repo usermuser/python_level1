cbdc70c64805a2f153b0e549b6efe1a7450071ab
usermuser <lwrdrstv@gmail.com>
2018-09-22 17:51:18 +0500

    убрал лишние файлы

--- lesson6/normal.py
+++ lesson6/normal.py
@@ -13,51 +13,72 @@ import random
 # Создайте экземпляры классов, проведите бой. Кто будет атаковать первым оставляю на ваше усмотрение.
 
 class Person():
-    def __init__(self, name, health, damage, armor):
+    def __init__(self, name, health=100):
         self.name = name
         self.health = health
-        self.damage = damage
-        self.armor = armor
+        self.damage = self.generate_damage()
+        self.armor = self.generate_armor()
 
+    def generate_armor(self):
+        self.armor = random.randint(1, 5)
+        return self.armor
+
+    def generate_damage(self):
+        self.damage = random.randint(5, 15)
+        return self.damage
+
+    def _calculate_damage(self, armor):
+        return self.damage // armor
+
+    def attack(self, victim):
+        damage = self._calculate_damage(victim.armor)
+        victim.health -= damage
+        print('{} нанес {} урона,'
+              ' у {} осталось {} жизней '.format(self.name, damage, victim.name, victim.health))
 
 class Player(Person):
+
     def battle_cry(self):
         return 'Boom shaka laka!'
 
 
 class Enemy(Person):
+
     def battle_cry(self):
         return 'Slamma jamma!'
 
+class Game():
+    def __init__(self, player, enemy):
+        self.player = player
+        self.enemy = enemy
 
-def calculate_damage(damage, armor):
-    return damage // armor
+    # позволим рандому определить того, кто будет ходить первым
+    def first_move(self, player1_name, player2_name):
+        tmp = [player1_name, player2_name]
+        return random.choice(tmp)
 
+    # игроки по-очереди атакуют друг-друга
+    def start_game(self):
+        attacker = self.first_move(self.player.name, self.enemy.name)
+        while player.health > 0 and enemy.health > 0:
 
-def attack(attacker, victim):
-    damage = calculate_damage(attacker.damage, victim.armor)
-    victim.health -= damage
-    print('{} нанес {} урона,'
-          ' у {} осталось {} жизней '.format(attacker.name, damage, victim.name, victim.health))
+            if attacker == player.name:
+                player.attack(enemy)    # игрок player атакует игрока enemy
+                attacker = enemy.name
+            else:
+                enemy.attack(player)    # игорок enemy атакует игрока player
+                attacker = player.name
 
-
-def start_game():
-    attacker = enemy
-    while player.health > 0 and enemy.health > 0:
-        if attacker == player:
-            attack(player, enemy)
-            attacker = enemy
+        # определение победителя
+        if player.health > 0:
+            print(player.name, 'с криком {} победил!'.format(player.battle_cry()))
         else:
-            attack(enemy, player)
-            attacker = player
-
-    if player.health > 0:
-        print(player.name, 'с криком {} победил!'.format(player.battle_cry()))
-    else:
-        print(enemy.name, 'с криком {} победил!'.format(enemy.battle_cry()))
+            print(enemy.name, 'с криком {} победил!'.format(enemy.battle_cry()))
 
 
-player = Player('Крушила', random.randint(50, 150), random.randint(5, 15), random.randint(1, 5))
-enemy = Enemy('Шустрик', random.randint(50, 150), random.randint(5, 15), random.randint(1, 5))
+if __name__ == '__main__':
 
-start_game()
+    player = Player('Крушила')
+    enemy = Enemy('Шустрик', 120 )
+    new_game = Game(player, enemy)
+    new_game.start_game()
