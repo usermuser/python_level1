3a2ff6733933a2e5ac9ce0028963a4dba714f6b7
usermuser <lwrdrstv@gmail.com>
2018-10-01 14:56:22 +0500

    Добавил проверки наличия номера в карточке, делаю логику игры, еще не доделал

--- lesson7/apollo440.py
+++ lesson7/apollo440.py
@@ -41,7 +41,7 @@ class Person():
 
 class Player(Person):
     def __init__(self):
-        self.card = Card().create_card()
+        self._card = Card().create_card()
         # self.card = copy.deepcopy(self.card_init)
         self._card_name = '------ Ваша карточка -----'
         # print('Player instance')
@@ -50,53 +50,109 @@ class Player(Person):
     def show_card(self):
         print(self._card_name)
         line_len = len(self._card_name)
-        for i in self.card:
+        for i in self._card:
             print(' '.join(i).rjust(line_len, ' '))
         print('--------------------------')
 
 
 class Cpu(Person):
     def __init__(self):
-        self.card_init = Card().create_card()
-        self.card = copy.deepcopy(self.card_init)
+        self._card = Card().create_card()
+        # self.card = copy.deepcopy(self.card_init)
         self._card_name = '-- Карточка компьютера ---'
-        print('Cpu instance')
+        # print('Cpu instance')
 
     def show_card(self):
         print(self._card_name)
         line_len = len(self._card_name)
-        for i in self.card:
+        for i in self._card:
             print(' '.join(i).rjust(line_len, ' '))
         print('--------------------------')
 
+
 class Bag():
     def __init__(self):
-        self.barrels = [x for x in range(1, 91)]
+        self._barrels = [x for x in range(1, 91)]
 
-    def create_bag(self):
-        while len(self.barrels):
-            barrel = self.barrels.pop(random.randrange(len(self.barrels)))
+    def _bag(self):
+        while len(self._barrels):
+            barrel = self._barrels.pop(random.randrange(len(self._barrels)))
             yield barrel
 
-    def next_barrel(self):
-        return next(self.create_bag())
+    def get_barrel(self):
+        return next(self._bag())
+
 
 class Game():
-    def __init__(self, player, cpu):
+    def __init__(self, player, cpu, bag):
         self.player = player
         self.cpu = cpu
+        self.bag = bag
+
 
     def start(self):
-        pass
+        exit = False
+
+        while not exit:
+            _barrel = bag.get_barrel()
+            print('Выпал бочонок', _barrel)
+
+            self.player.show_card()
+            self.cpu.show_card()
+
+            _answer = self.ask()
+            barrel_in_card = self.check_ok(_barrel)
+
+            if _answer and barrel_in_card:
+                self.player._card[self.player._card.index(_barrel)] = '-'
+
+            elif _answer and not barrel_in_card:
+                print('В твоей карточке нет такого номера!')
+                exit = True
+
+
+
+
+
+
+    def check_ok(self, _barrel):
+        if _barrel in self.player._card:
+            return True
+        else:
+            return False
+
+    def ask(self):
+        ask_exit = False
+        while not ask_exit:
+            answer = int(input('Что вы хотите сделать?\n'
+                           'Нажмите 1 если хотите зачеркнуть\n'
+                           'Нажмите 2 если хотите продолжить\n'
+                            'Нажмите 3 для выхода'))
+            if answer == 3:
+                ask_exit = True
+            elif answer == 1:
+                return True
+            elif answer == 2:
+                return False
+            else:
+                print('Вы ввели что-то не то')
+
+
+
+
+
 
 
 if __name__ == '__main__':
 
     player = Player()
-    player.show_card()
-
     cpu = Cpu()
-    cpu.show_card()
+    bag = Bag()
 
-    game = Game(player, cpu)
+    # player.show_card()
+    # cpu.show_card()
+
+
+
+    game = Game(player, cpu, bag)
     game.start()
